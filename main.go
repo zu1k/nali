11dd415c35bf989bc3a74cbe5f3cd2c94a5522f5
zu1k <i@lgf.im>
2020-07-22 06:32:13 +0800

    specific db homedir

--- main.go
+++ main.go
@@ -15,11 +15,14 @@ func main() {
 }
 
 func setHomePath() {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		panic(err)
+	homePath := os.Getenv("NALI_DB_HOME")
+	if homePath == "" {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			panic(err)
+		}
+		homePath = filepath.Join(homeDir, ".nali")
 	}
-	homePath := filepath.Join(homeDir, ".nali")
 	constant.HomePath = homePath
 	if _, err := os.Stat(homePath); os.IsNotExist(err) {
 		if err := os.MkdirAll(homePath, 0777); err != nil {
