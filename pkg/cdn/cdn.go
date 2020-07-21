d5d766f5362f950d1b8065efa449ffeeb744aa11
zu1k <i@lgf.im>
2020-07-22 07:25:01 +0800

    update all db

--- pkg/cdn/cdn.go
+++ pkg/cdn/cdn.go
@@ -22,22 +22,14 @@ func NewCDN(filePath string) CDN {
 	cdnDist := make(CDNDist)
 	cdnData := make([]byte, 0)
 
-	// 判断文件是否存在
 	_, err := os.Stat(filePath)
 	if err != nil && os.IsNotExist(err) {
 		log.Println("文件不存在，尝试从网络获取最新CDN数据库")
-		cdnData, err = Download()
+		cdnData, err = Download(filePath)
 		if err != nil {
-			log.Printf("CDN数据库下载失败，请手动下载解压后保存到本地: %s \n", filePath)
-			log.Println("下载链接： https://cdn.jsdelivr.net/gh/SukkaLab/cdn/dist/cdn.json")
 			os.Exit(1)
-		} else {
-			if err := ioutil.WriteFile(filePath, cdnData, 0644); err == nil {
-				log.Printf("已将最新的 CDN数据库 保存到本地: %s \n", filePath)
-			}
 		}
 	} else {
-		// 打开文件句柄
 		cdnFile, err := os.OpenFile(filePath, os.O_RDONLY, 0400)
 		if err != nil {
 			panic(err)
--- pkg/cdn/update.go
+++ pkg/cdn/update.go
@@ -2,10 +2,28 @@ package cdn
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/zu1k/nali/pkg/common"
 )
 
-func Download() (data []byte, err error) {
+func Download(filePath string) (data []byte, err error) {
+	data, err = getData()
+	if err != nil {
+		log.Printf("CDN数据库下载失败，请手动下载解压后保存到本地: %s \n", filePath)
+		log.Println("下载链接： https://cdn.jsdelivr.net/gh/SukkaLab/cdn/dist/cdn.json")
+		return
+	}
+
+	common.ExistThenRemove(filePath)
+	if err := ioutil.WriteFile(filePath, data, 0644); err == nil {
+		log.Printf("已将最新的 CDN数据库 保存到本地: %s \n", filePath)
+	}
+	return
+}
+
+func getData() (data []byte, err error) {
 	//resp, err := http.Get("https://raw.githubusercontent.com/SukkaLab/cdn/master/dist/cdn.json")
 	resp, err := http.Get("https://cdn.jsdelivr.net/gh/SukkaLab/cdn/dist/cdn.json")
 	if err != nil {
