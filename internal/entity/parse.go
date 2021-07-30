cd68efc95633746da772edc4840d5a42a7c49fc6
zu1k <i@lgf.im>
2021-07-30 21:46:06 +0800

    Add type Entity

---
+++ internal/entity/parse.go
@@ -0,0 +1,10 @@
+package entity
+
+// ParseLine parse a line into entities
+func ParseLine(line string) Entities {
+	entities := make(Entities, 0)
+
+	// TODO: parse a line into entities
+
+	return entities
+}
