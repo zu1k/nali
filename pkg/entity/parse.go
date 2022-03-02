268e6f4bdb3be9c15c9255ee9ffb81fe298f5a70
zu1k <i@lgf.im>
2022-03-02 12:51:21 +0800

    docs: Update readme
    Signed-off-by: zu1k <i@lgf.im>

--- pkg/entity/parse.go
+++ pkg/entity/parse.go
@@ -4,10 +4,9 @@ import (
 	"net/netip"
 	"sort"
 
-	"github.com/zu1k/nali/pkg/re"
-
 	"github.com/zu1k/nali/internal/db"
 	"github.com/zu1k/nali/pkg/dbif"
+	"github.com/zu1k/nali/pkg/re"
 )
 
 // ParseLine parse a line into entities
