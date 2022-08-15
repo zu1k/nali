8307d1407511498356bb7fa9f6ca397d40178c09
zu1k <i@zu1k.com>
2022-08-15 09:11:10 +0800

    refactor: Simplified ScanLines
    Signed-off-by: zu1k <i@zu1k.com>

--- pkg/common/scan.go
+++ pkg/common/scan.go
@@ -9,27 +9,14 @@ func ScanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	if atEOF && len(data) == 0 {
 		return 0, nil, nil
 	}
-	if i, j := bytes.IndexByte(data, '\r'), bytes.IndexByte(data, '\n'); i >= 0 || j >= 0 {
-		if i >= 0 && j >= 0 {
-			if i+1 == j {
-				// case 1: TOKEN\r\nTOKEN
-				return i + 2, data[:i+2], nil
-			}
-			if i < j {
-				// case 2: TOKEN\rTOKEN\nTOKEN
-				return i + 1, data[:i+1], nil
-			} else {
-				// case 3: TOKEN\nTOKEN\rTOKEN
-				return j + 1, data[:j+1], nil
-			}
-		} else if i >= 0 {
-			// case 4: TOKEN\rTOKEN
-			return i + 1, data[:i+1], nil
-		} else {
-			// case 5: TOKEN\nTOKEN
-			return j + 1, data[:j+1], nil
-		}
+
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		return i + 1, data[:i+1], nil
 	}
+	if i := bytes.IndexByte(data, '\r'); i >= 0 {
+		return i + 1, data[:i+1], nil
+	}
+
 	// If we're at EOF, we have a final, non-terminated line. Return it.
 	if atEOF {
 		return len(data), data, nil
